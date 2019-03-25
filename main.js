237d4b0f15846651a78994f4a00d904a28d26b61
Gerard Hynes <gkhynes@gmail.com>
2019-03-25 22:40:14 +0000

    Add scrolling functionality

---
+++ main.js
@@ -0,0 +1,51 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current slide
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // Add current to start
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => {
+    current.classList.remove("current");
+  });
+};
+
+const prevSlide = () => {
+  // Get current slide
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for previous slide
+  if (current.previousElementSibling) {
+    // Add current to previous sibling
+    current.previousElementSibling.classList.add("current");
+  } else {
+    // Add current to last slide
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => {
+    current.classList.remove("current");
+  });
+};
+
+// Button events
+nextBtn.addEventListener("click", e => {
+  nextSlide();
+});
+
+prevBtn.addEventListener("click", e => {
+  prevSlide();
+});
